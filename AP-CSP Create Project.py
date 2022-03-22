3d0532f588570d57d0307652af4978ff260305af
Saket_Poludasu <ravagesparc123@gmail.com>
2022-03-21 19:41:50 -0700

    a

--- AP-CSP Create Project.py
+++ AP-CSP Create Project.py
@@ -1,16 +1,41 @@
 import pygame, sys
+
+clock = pygame.time.Clock()
+
 from pygame.locals import *
 pygame.init()
 
-windowSize = (400, 400)
+pygame.display.set_caption('Create Project Dungeon Crawler')
+
+windowSize = (1100, 700)
 
 screen = pygame.display.set_mode(windowSize, 0, 32)
 
+swordIMG = pygame.image.load('sword2.png')
+evilFaceIMG = pygame.image.load('evilFace1.png')
+
+movingRight = False
+movingLeft = False
+
 while True:
+
+    screen.blit(swordIMG, (550, 350))
+    screen.blit(evilFaceIMG, (100, 100))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.ext()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_d:
+                movingRight = True
+            if event.key == K_a:
+                movingLeft = True
+        if event.type == KEYUP:
+            if event.key == K_d:
+                movingRight = False
+            if event.key == K_a:
+                movingLeft = False
 
     pygame.display.update()
-
+    clock.tick(60)
