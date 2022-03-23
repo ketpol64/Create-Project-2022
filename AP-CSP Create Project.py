4d68292238562fd1503b6e817fdaa71c493c4a9b
Saket_Poludasu <ravagesparc123@gmail.com>
2022-03-22 17:18:40 -0700

    a

--- AP-CSP Create Project.py
+++ AP-CSP Create Project.py
@@ -1,17 +1,15 @@
 import pygame,sys
-
-clock = pygame.time.Clock()
-
 from pygame.locals import *
 pygame.init()
 
+clock = pygame.time.Clock()
+
 pygame.display.set_caption('Create Project Dungeon Crawler')
-
 windowSize = (1100, 700)
-
 screen = pygame.display.set_mode(windowSize, 0, 32)
 
 mainCharIMG = pygame.image.load('images/heroFace1.png')
+
 
 movingRight = False
 movingLeft = False
@@ -20,16 +18,13 @@ playerYMom = 0
 playerHB = Rect(playerLoc[0], playerLoc[1], mainCharIMG.get_width(), mainCharIMG.get_height())
 
 while True:
-
     screen.fill((52, 174, 235))
-
     screen.blit(mainCharIMG, playerLoc)
 
     if movingRight:
         playerLoc[0] += 10
     if movingLeft:
         playerLoc[0] -= 10
-    
     playerYMom += 0.2
     playerLoc[1] += playerYMom
 
