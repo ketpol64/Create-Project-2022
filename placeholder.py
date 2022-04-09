2abd4fbe13d5deff4ca46e8dcfe693b2d5ed4daa
Saket_Poludasu <ravagesparc123@gmail.com>
2022-04-08 18:24:33 -0700

    a

--- placeholder.py
+++ placeholder.py
@@ -1,24 +1,21 @@
+from tkinter import font
 import pygame, sys
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('2022 AP-CSP Create Project: Isometric Dungeon Crawler')
 
 gameFrameCount = pygame.time.Clock()
 
-from pygame.locals import *
-pygame.init()
-
-pygame.display.set_caption('2022 AP-CSP Create Project: Isometric Dungeon Crawler')
-
 windowSize = (1100,700)
-
 windowBase = pygame.display.set_mode(windowSize,0,32)
 
-display = pygame.Surface((550,350))
+font20 = pygame.font.SysFont(None,20)
 
-movingRight = False
-movingLeft = False
-verticalMom = 0
-airTime = 0
-
-trueScroll = [0,0]
+def drawText(text,font,color,surface,x,y):
+    textObj = font.render(text,1,color)
+    textRect = textObj.get_rect()
+    textRect.topleft = (x,y)
+    surface.blit(textObj,textRect)
 
 def loadMap(path):
     f = open(path + '.txt','r')
@@ -29,9 +26,6 @@ def loadMap(path):
     for row in data:
         gameMap.append(list(row))
     return gameMap
-
-global animationFrames
-animationFrames = {}
 
 def loadAnimation(path,frameCount):
     global animationFrames
@@ -54,25 +48,6 @@ def changeAction(currentAction,frame,newAction):
         currentAction = newAction
         frame = 0
     return currentAction,frame
-
-animationDatabase = {}
-
-animationDatabase['idle'] = loadAnimation('animations/player1/idle',[7,7,7,7])
-animationDatabase['running'] = loadAnimation('animations/player1/running',[7,7])
-
-currentPlayerAction = 'idle'
-playerFrame = 0
-playerFlip = False
-
-gameMap = loadMap('maps/map1')
-
-grassImg = pygame.image.load('images/grass1.png')
-dirtImg = pygame.image.load('images/dirt1.png')
-tileSize = grassImg.get_width()
-
-playerHB = pygame.Rect(50,50,17,36)
-
-backgroundObjects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,40,40,400]],[0.5,[130,90,100,400]],[0.5,[300,80,120,400]]]
 
 def collisionTest(rect,tiles):
     hitList = []
@@ -103,91 +78,150 @@ def move(rect,movement,tiles):
             collisionTypes['top'] = True
     return rect, collisionTypes
 
-while True:
-    display.fill((146,244,255))
+def lOne(levelOne):
+    display = pygame.Surface((550,350))
 
-    trueScroll[0] += (playerHB.x-trueScroll[0]-152)/20
-    trueScroll[1] += (playerHB.y-trueScroll[1]-106)/20
-    scroll = trueScroll.copy()
-    scroll[0] = int(scroll[0])
-    scroll[1] = int(scroll[1])
+    movingRight = False
+    movingLeft = False
+    verticalMom = 0
+    airTime = 0
+    trueScroll = [0,0]
 
-    pygame.draw.rect(display,(7,80,75),pygame.Rect(0,150,550,200))
-    for backgroundObject in backgroundObjects:
-        objRect = pygame.Rect(backgroundObject[1][0]-scroll[0]*backgroundObject[0],backgroundObject[1][1]-scroll[1]*backgroundObject[0],backgroundObject[1][2],backgroundObject[1][3])
-        if backgroundObject[0] == 0.5:
-            pygame.draw.rect(display,(14,222,150),objRect)
+    global animationFrames
+    animationFrames = {}
+    animationDatabase = {}
+    animationDatabase['idle'] = loadAnimation('animations/player1/idle',[7,7,7,7])
+    animationDatabase['running'] = loadAnimation('animations/player1/running',[7,7,7,7]) 
+    currentPlayerAction = 'idle'
+    playerFrame = 0
+    playerFlip = False
+
+    gameMap = loadMap('maps/map1')
+    grassImg = pygame.image.load('images/grass1.png')
+    dirtImg = pygame.image.load('images/dirt1.png')
+    tileSize = grassImg.get_width()
+    backgroundObjects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,40,40,400]],[0.5,[130,90,100,400]],[0.5,[300,80,120,400]]]
+
+    playerHB = pygame.Rect(200,50,17,36)
+
+    while levelOne:
+        display.fill((146,244,255))
+
+        trueScroll[0] += (playerHB.x-trueScroll[0]-152)/20
+        trueScroll[1] += (playerHB.y-trueScroll[1]-106)/20
+        scroll = trueScroll.copy()
+        scroll[0] = int(scroll[0])
+        scroll[1] = int(scroll[1])
+
+        pygame.draw.rect(display,(7,80,75),pygame.Rect(0,150,550,200))
+        for backgroundObject in backgroundObjects:
+            objRect = pygame.Rect(backgroundObject[1][0]-scroll[0]*backgroundObject[0],backgroundObject[1][1]-scroll[1]*backgroundObject[0],backgroundObject[1][2],backgroundObject[1][3])
+            if backgroundObject[0] == 0.5:
+                pygame.draw.rect(display,(14,222,150),objRect)
+            else:
+                pygame.draw.rect(display,(9,91,85),objRect)
+
+        tileRects = []
+        y = 0
+        for layer in gameMap:
+            x = 0
+            for tile in layer:
+                if tile == '1':
+                    display.blit(grassImg,(x*tileSize-scroll[0],y*tileSize-scroll[1]))
+                if tile == '2':
+                    display.blit(dirtImg,(x*tileSize-scroll[0],y*tileSize-scroll[1]))
+                if tile != '0':
+                    tileRects.append(pygame.Rect(x*tileSize,y*tileSize,tileSize,tileSize))
+                x += 1
+            y += 1
+
+        playerMovement = [0,0]
+        if movingRight:
+            playerMovement[0] += 3
+        if movingLeft:
+            playerMovement[0] -= 3
+        playerMovement[1] += verticalMom
+        verticalMom += 0.2
+        if verticalMom > 3:
+            verticalMom = 3
+
+        if playerMovement[0] > 0:
+            currentPlayerAction,playerFrame = changeAction(currentPlayerAction,playerFrame,'running')
+            playerFlip = False
+        if playerMovement[0] == 0:
+            currentPlayerAction,playerFrame = changeAction(currentPlayerAction,playerFrame,'idle')
+        if playerMovement[0] < 0:
+            currentPlayerAction,playerFrame = changeAction(currentPlayerAction,playerFrame,'running')
+            playerFlip = True
+
+        playerHB,collisions = move(playerHB,playerMovement,tileRects)
+        if collisions['bottom']:
+            airTime = 0
+            verticalMom = 0
         else:
-            pygame.draw.rect(display,(9,91,85),objRect)
+            airTime += 1
+        if collisions['top']:
+            verticalMom = 0
 
-    tileRects = []
-    y = 0
-    for layer in gameMap:
-        x = 0
-        for tile in layer:
-            if tile == '1':
-                display.blit(grassImg,(x*tileSize-scroll[0],y*tileSize-scroll[1]))
-            if tile == '2':
-                display.blit(dirtImg,(x*tileSize-scroll[0],y*tileSize-scroll[1]))
-            if tile != '0':
-                tileRects.append(pygame.Rect(x*tileSize,y*tileSize,tileSize,tileSize))
-            x += 1
-        y += 1
+        playerFrame += 1
+        if playerFrame >= len(animationDatabase[currentPlayerAction]):
+            playerFrame = 0
+        playerImgID = animationDatabase[currentPlayerAction][playerFrame]
+        playerImg = animationFrames[playerImgID]
+        display.blit(pygame.transform.flip(playerImg,playerFlip,False),(playerHB.x-scroll[0],playerHB.y-scroll[1]))
 
-    playerMovement = [0,0]
-    if movingRight:
-        playerMovement[0] += 3
-    if movingLeft:
-        playerMovement[0] -= 3
-    playerMovement[1] += verticalMom
-    verticalMom += 0.2
-    if verticalMom > 3:
-        verticalMom = 3
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    levelOne = False
+                if event.key == K_d:
+                    movingRight = True
+                if event.key == K_a:
+                    movingLeft = True
+                if event.key == K_SPACE:
+                    if airTime < 20:
+                        verticalMom = -5
+            if event.type == KEYUP:
+                if event.key == K_d:
+                    movingRight = False
+                if event.key == K_a:
+                    movingLeft = False
 
-    if playerMovement[0] > 0:
-        currentPlayerAction,playerFrame = changeAction(currentPlayerAction,playerFrame,'running')
-        playerFlip = False
-    if playerMovement[0] == 0:
-        currentPlayerAction,playerFrame = changeAction(currentPlayerAction,playerFrame,'idle')
-    if playerMovement[0] < 0:
-        currentPlayerAction,playerFrame = changeAction(currentPlayerAction,playerFrame,'running')
-        playerFlip = True
+        windowBase.blit(pygame.transform.scale(display,windowSize),(0,0))
+        pygame.display.update()
+        gameFrameCount.tick(60)
 
-    playerHB,collisions = move(playerHB,playerMovement,tileRects)
+def mainMenu():
+    while True:
+        windowBase.fill((146,244,255))
+        drawText('Main Menu',font20,(255,255,255),windowBase,20,20)
 
-    if collisions['bottom']:
-        airTime = 0
-        verticalMom = 0
-    else:
-        airTime += 1
-    if collisions['top']:
-        verticalMom = 0
-    
-    playerFrame += 1
-    if playerFrame >= len(animationDatabase[currentPlayerAction]):
-        playerFrame = 0
-    playerImgID = animationDatabase[currentPlayerAction][playerFrame]
-    playerImg = animationFrames[playerImgID]
-    display.blit(pygame.transform.flip(playerImg,playerFlip,False),(playerHB.x-scroll[0],playerHB.y-scroll[1]))
+        mx,my = pygame.mouse.get_pos()
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_d:
-                movingRight = True
-            if event.key == K_a:
-                movingLeft = True
-            if event.key == K_SPACE:
-                if airTime < 20:
-                    verticalMom = -5
-        if event.type == KEYUP:
-            if event.key == K_d:
-                movingRight = False
-            if event.key == K_a:
-                movingLeft = False
-        
-    windowBase.blit(pygame.transform.scale(display,windowSize),(0,0))
-    pygame.display.update()
-    gameFrameCount.tick(60)
+        b1 = pygame.Rect(50,100,200,50)
+        b2 = pygame.Rect(50,200,200,50)
+        if b1.collidepoint((mx,my)):
+            if click:
+                lOne(True)
+#        if b2.collidepoint((mx,my)):
+#            if click:
+#                pass
+        pygame.draw.rect(windowBase,(255,255,255),b1)
+        pygame.draw.rect(windowBase,(255,255,255),b2)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        gameFrameCount.tick(60)
+
+mainMenu()
